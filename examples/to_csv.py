97a82a8cfcd2c4febac17b30b3b45bd63c1108a1
Paul Jena <paul.jena@optimeas.de>
2023-09-27 20:30:11 +0200

    Fix uncaught error in to_csv example if a file with an unsupported file format was passed

--- examples/to_csv.py
+++ examples/to_csv.py
@@ -13,11 +13,17 @@ def main(argv: list[str]):
     parser.add_argument('-c', action='append', dest='channels')
 
     args = parser.parse_args()
-    path = Path(args.input) 
+    if not args.input:
+        print('You need to specify a file or directory via the -i flag')
+        parser.print_help()
+        return
     
     if not args.channels:
         print('You need to specify atleast one channel via the -c flag')
+        parser.print_help()
         return
+
+    path = Path(args.input) 
 
     if path.is_file():
         with libosf.read_file(path) as f:
@@ -47,4 +53,7 @@ def main(argv: list[str]):
         print('Can not read in anything other than a file or directory')
 
 if __name__ == '__main__':
-    main(sys.argv)
+    try:
+        main(sys.argv[1:])
+    except RuntimeError as e:
+        print(f'err: {e}')
