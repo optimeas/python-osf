10f9bcd10c7a83d5b48f73c128c085134a45c7df
Paul Jena <paul.jena@optimeas.de>
2023-10-19 16:44:34 +0200

    Fix crash in csv_example

--- examples/metadata.py
+++ examples/metadata.py
@@ -7,6 +7,8 @@ OSF_FILE = EXAMPLE_DIR.joinpath("example.osf")
 
 def main():
     with read_file(OSF_FILE) as f:
+        metadata = f.metadata()
+        print(f"{metadata.creator}")
         channels = f.channels()
         for i, c in enumerate(channels):
             print(f"ch{i}: {c.name}, unit: {c.unit}, type: {c.type}")
--- examples/to_csv.py
+++ examples/to_csv.py
@@ -1,61 +1,62 @@
-import numpy as np 
+import numpy as np
 import libosf
 from argparse import ArgumentParser
 import pandas as pd
 from pathlib import Path
 import sys
-import os
 
 EXAMPLE_DIR = Path(__file__).absolute().parent
 OSF_FILE = EXAMPLE_DIR.joinpath("example.osf")
 
+
 def main(argv: list[str]):
-    parser = ArgumentParser('numpy to csv example', description='pass an file or directory to dump to a csv file')
-    parser.add_argument('-i', action='store', dest='input')
-    parser.add_argument('-c', action='append', dest='channels')
+    parser = ArgumentParser(
+        "numpy to csv example",
+        description="pass an file or directory to dump to a csv file",
+    )
+    parser.add_argument("-i", action="store", dest="input")
+    parser.add_argument("-c", action="append", dest="channels")
 
     args = parser.parse_args()
     if not args.input:
-        print('You need to specify a file or directory via the -i flag')
+        print("You need to specify a file or directory via the -i flag")
         parser.print_help()
         return
-    
     if not args.channels:
-        print('You need to specify atleast one channel via the -c flag')
+        print("You need to specify atleast one channel via the -c flag")
         parser.print_help()
         return
 
-    path = Path(args.input) 
+    path = Path(args.input)
+    print(path)
 
     if path.is_file():
-        with libosf.read_file(path) as f:
-            samples = f.get_samples(args.channels)
-            data = {
-                    'ts_n': samples[0],
-                    'value': samples[1],
-                    'ch_index': samples[2]
-            }
+        with libosf.read_file(path) as osf_file:
+            samples = osf_file.get_samples(args.channels)
+            data = {"ts_n": samples[0], "value": samples[1]}
             df = pd.DataFrame(data=data)
-            df.to_csv('output.csv')
-            return
-    if path.is_dir():
-        result_array = None
+            with open(
+                f"{path.with_suffix('').with_suffix('.csv').absolute()}", "w+"
+            ) as f:
+                f.write("This is a great line\n")
+                df.to_csv(f, sep=";")
+    elif path.is_dir():
         for child in path.iterdir():
-            samples = f.get_samples(args.channels)
-            data = {
-                    'ts_n': samples[0],
-                    'value': samples[1],
-                    'ch_index': samples[2]
-            }
-            df = pd.DataFrame(data=data)
-
-        df.to_csv(f'{child.with_suffix("").with_suffix(".csv").absolute()}')
-        
+            with libosf.read_file(path) as osf_file:
+                samples = osf_file.get_samples(args.channels)
+                data = {"ts_n": samples[0], "value": samples[1]}
+                df = pd.DataFrame(data=data)
+                with open(
+                    f"{child.with_suffix('').with_suffix('.csv').absolute()}", "w+"
+                ) as f:
+                    f.write("This is a great line\n")
+                    df.to_csv(f, sep=";")
     else:
-        print('Can not read in anything other than a file or directory')
+        print("Can not read in anything other than a file or directory")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         main(sys.argv[1:])
     except RuntimeError as e:
-        print(f'err: {e}')
+        print(f"err: {e}")
